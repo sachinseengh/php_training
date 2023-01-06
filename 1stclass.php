24498d7219485cb92f859290ec479524cc9f73c8
sachinseengh <sachinseengh@gmail.com>
2023-01-06 11:36:47 +0545

    phpcrud

--- 1stclass.php
+++ 1stclass.php
@@ -1,16 +1,28 @@
 <?php
 
 class Users{
-    public $id,$name,$address;
+    public $id,$name,$phone;
 
-public function message( ){
-    echo "message box";
+// public function message( ){
+//     echo "message box";
+// }
+// public function insert(){
+
+// }
+public function __construct($value,$id,$phone){
+    $this->name=$value;
+    $this->id=$id;
+    $this->phone=$phone;
 }
 }
 
 
-$object = new Users( );
-print_r($object->message( ));
+$object = new Users('sachin',1,95884658 );
+// print_r($object->message( ));
+// print_r($object->insert());
+print_r($object);
 
 
 ?>
+//access modifier are of three type
+//protected,private and public
